49d7f1615fb253a2aadf6ec58e9f0763a2d70f78
emilijasimic1510 <emilija1510.s@gmail.com>
2025-08-14 14:35:14 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -9,11 +9,11 @@ Script Purpose:
 ===============================================================================
 */
 
-IF OBJECT_ID('bronze.crm_customer_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_customer_info;
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
 GO
 
-CREATE TABLE bronze.crm_customer_info (
+CREATE TABLE bronze.crm_cust_info (
     cst_id              INT,
     cst_key             NVARCHAR(50),
     cst_firstname       NVARCHAR(50),
